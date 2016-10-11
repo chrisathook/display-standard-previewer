9839f8f0c8a8da8b2ddeb9aa7f2571e53f089563
Christopher Watts <chris@byhook.com>
2016-10-11 13:15:21 -0700

    getting watch tasks working

--- display-standard-previewer.js
+++ display-standard-previewer.js
@@ -3,35 +3,45 @@
 var path = require('path');
 var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
-
-var gulpTasks = require ( './gulp');
-
+var gulpTasks = require('./gulp');
 var opts = {
-	logLevel: 2,
-  home:'.'
+  logLevel: 2,
+  home: '.'
 };
-
-var homeDir = '';
-
-
-
-var run = function (){
-
-  console.log ("Hello World");
-  console.log (process.cwd());
-  console.log (__filename );
-  console.log (__dirname );
+var task = "default";
+var run = function () {
+  console.log("Hello World");
+  console.log(process.cwd());
+  console.log(__filename);
+  console.log(__dirname);
   console.log(argv);
-
-
   opts.home = process.cwd();
 
 
-  gulpTasks()
-    .then (function (){console.log ('resolved')})
+  task = parseTask(argv._);
 
-
+  gulpTasks(opts, task)
+    .then(function () {
+      console.log('resolved')
+    })
 };
+var parseTask = function (paramArray) {
+  if (paramArray.indexOf('watch') !== -1) {
+    return 'watch'
+  }
+  if (paramArray.indexOf('sass') !== -1) {
+    return 'sass'
+  }
+  if (paramArray.indexOf('sprite') !== -1) {
+    return 'sprite'
+  }
+  if (paramArray.indexOf('build') !== -1) {
+    return 'build'
+  }
+  if (paramArray.indexOf('end-watch') !== -1) {
+      return 'end-watch'
+    }
 
-
+  return 'default'
+};
 run();
--- gulp/index.js
+++ gulp/index.js
@@ -1,26 +1,66 @@
 'use strict';
-
 var gulp = require('gulp');
 var config = require('./config');
 var bs = require('browser-sync').create();
-
 module.exports = function (opts, task) {
   return new Promise(function (resolve, reject) {
-
-
-
-
     gulp.task('resolve', function (done) {
       console.log('gulp done');
       resolve();
       done();
     });
-
     gulp.task('sass', require('./tasks/lib-sass')(gulp, bs, config.sass, config.flags));
     gulp.task('sprite-collapsed-foreground', require('./tasks/sprite-images')(gulp, bs, config.sprite.collapsed_foreground, config.flags));
     gulp.task('sprite-collapsed-background', require('./tasks/sprite-images')(gulp, bs, config.sprite.collapsed_background, config.flags));
     gulp.task('sprite-all', gulp.parallel('sprite-collapsed-foreground', 'sprite-collapsed-background'));
+    // define watch actions
+    gulp.task('watch', function (done) {
+      var callback = function () {
+        resolve();
+      };
+      bs.init({
+        server: {
+          baseDir: config.server.root
+        },
+        online: false,
+        port: config.server.port,
+        reloadDelay: 1000,
+        reloadDebounce: 1000,
+        ghostMode: {
+          clicks: false,
+          forms: false,
+          scroll: false
+        }
+      }, callback);
+      gulp.watch(config.sass.watch_src, gulp.series('sass')); // only watch files not associated with spritesheets
+      // generate new sass partials for spritesheets when images edited
+      gulp.watch(config.sprite.collapsed_foreground.src, gulp.series('sprite-collapsed-foreground'));
+      gulp.watch(config.sprite.collapsed_background.src, gulp.series('sprite-collapsed-background'));
+      done();
+    });
+    if (task === 'watch') {
+      gulp.series('sprite-all', 'sass', 'watch').call();
+    } else if (task === 'end-watch') {
+      bs.exit ();
 
-    gulp.series('sprite-all','sass','resolve').call();
+    } else if (task === 'sass') {
+
+      gulp.series('sass', 'resolve').call();
+
+    } else if (task === 'sprite') {
+
+      gulp.series('sprite-all', 'resolve').call();
+
+    } else if (task === 'build') {
+
+      gulp.series('sprite-all', 'sass', 'resolve').call();
+
+    } else if (task === 'default') {
+
+      gulp.series('sprite-all', 'sass', 'resolve').call();
+
+    } else {
+      resolve()
+    }
   });
 };
